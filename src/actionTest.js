1a14ddb064e4483f8df1342ccbe7001dd126e858
KyungJun.Woo <natcho9010@gmail.com>
2024-10-01 22:45:27 +0900

    log commit interface itself

--- src/actionTest.js
+++ src/actionTest.js
@@ -8,22 +8,23 @@ async function run() {
 
     // GitHub Context에서 PR 정보를 가져옴
     const pullRequest = github.context.payload.pull_request;
-    
+
     // 머지하려는 브랜치와 머지 대상 브랜치 이름
     const baseBranch = pullRequest.base.ref;  // 머지 타겟 브랜치 (ex: main)
     const headBranch = pullRequest.head.ref;  // 머지하려는 원본 브랜치 (ex: feature/new-feature)
-    
+
     console.log(`Comparing changes between ${headBranch} and ${baseBranch}`);
-    
+
     // 두 브랜치 간의 커밋 차이 가져오기
     await git.fetch();
-    
+
     // 차이가 있는 커밋들 가져오기
     const commitsDiff = await git.log([`${baseBranch}..${headBranch}`]);
 
     console.log(`Commits between ${headBranch} and ${baseBranch}:`);
     commitsDiff.all.forEach(commit => {
-      console.log(`Commit: ${commit.hash}, Message: ${commit.message}, Author: ${commit.author_name}`);
+      console.log('commit',commit)
+      // console.log(`Commit: ${commit.hash}, Message: ${commit.message}, Author: ${commit.author_name}`);
     });
 
   } catch (error) {
