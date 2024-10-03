b6a5199017ab0083e93cd0ab36443441a7a63fbf
KyungJun.Woo <natcho9010@gmail.com>
2024-10-04 00:20:40 +0900

    add rollup

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,13 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+'use strict';
+
 Object.defineProperty(exports, "__esModule", { value: true });
-const core_1 = __importDefault(require("@actions/core"));
+const tslib_1 = require("tslib");
+const core_1 = tslib_1.__importDefault(require("@actions/core"));
 const git_diff_1 = require("./git_diff");
 const utils_1 = require("./utils");
 const github_1 = require("./github");
 function run() {
-    return __awaiter(this, void 0, void 0, function* () {
+    return tslib_1.__awaiter(this, void 0, void 0, function* () {
         const githubToken = core_1.default.getInput('GITHUB_TOKEN');
         const commentTitle = core_1.default.getInput('GIT_DIFF_COMMENT_TITLE');
         // git diff를 가져옴
