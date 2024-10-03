d14c2bde7b3bf147317485d8ac5888c7317af384
KyungJun.Woo <natcho9010@gmail.com>
2024-10-03 22:53:47 +0900

    change

--- src/actionTest.js
+++ src/actionTest.js
@@ -31,7 +31,7 @@ async function run() {
     console.log(`Base branch commit: ${baseBranchCommit}`);
     console.log(`Head branch commit: ${headBranchCommit}`);
 
-    const diff = git.diff([baseBranch, headBranch]);
+    const diff = git.log([`origin/${baseBranch}`, "HEAD"]);
     console.dir(diff);
     console.log('diff : ', diff);
 
@@ -40,7 +40,7 @@ async function run() {
       "--name-only",
       "--",
       "*.ts"
-    ], baseBranch, headBranch);
+    ], `origin/${baseBranch}`, "HEAD");
     console.dir(diffSummary);
     console.log('diffSummary : ', diffSummary);
 
