4ff00e2c4fa16c4b639ffe635054987cc28fa709
KyungJun.Woo <natcho9010@gmail.com>
2024-10-05 22:19:39 +0900

    change

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { upsertCommentInPullRequest } from './github';
 
 async function run() {
   // const githubToken = core.getInput('GITHUB_TOKEN');
-  const githubToken = process.env.GITHUB_TOKEN ?? '';
+  const githubToken = process.env.ACCESS_TOKEN ?? '';
   const commentTitle = core.getInput('GIT_DIFF_COMMENT_TITLE');
   console.log('githubToken',githubToken);
   console.log('commentTitle',commentTitle);
