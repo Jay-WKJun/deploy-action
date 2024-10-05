f0087b8385e8358e0399b3d64aa1a404cbe3ac95
KyungJun.Woo <natcho9010@gmail.com>
2024-10-05 23:09:49 +0900

    rtyu

--- dist/index.js
+++ dist/index.js
@@ -36523,10 +36523,12 @@ function run() {
             return;
         // git diff 내용을 이메일 별로 그룹화
         const gitDiffMap = gatherCommitsByEmail(gitDiffs);
+        console.log('gitDiffMap', gitDiffMap);
+        const commentBody = JSON.stringify(Array.from(gitDiffMap.entries()));
         // git diff를 PR 코멘트로 업데이트
         upsertCommentInPullRequest({
             githubToken,
-            commentBody: JSON.stringify(Object(gitDiffMap)),
+            commentBody,
             commentTitle: '추가됨',
         });
     });
