1fc14d7c261dbb0999cb967925e45250acd4c1a3
KyungJun.Woo <natcho9010@gmail.com>
2024-10-05 23:43:58 +0900

    finished

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,47 @@
 import core from '@actions/core';
+import github from '@actions/github';
 import { getGitDiff } from "./git_diff";
-import { gatherCommitsByEmail } from "./utils";
+import { gatherCommitsByEmail, getCommitUrl } from "./utils";
 import { upsertCommentInPullRequest } from './github';
 
 async function run() {
+  // GitHub 이벤트 이름을 가져옴
+  const eventName = github.context.eventName;
 
-  // const githubToken = core.getInput('GITHUB_TOKEN');
-  console.log('process.env', process.env);
-  const githubToken = process.env.GITHUB_TOKEN ?? '';
-  const githubRepository = process.env.GITHUB_REPOSITORY ?? '';
-  const githubRef = process.env.GITHUB_REF ?? '';
-  const prNumber = githubRef?.split('/')[2] ?? '';
-  // const commentTitle = core.getInput('GIT_DIFF_COMMENT_TITLE');
-  console.log('githubToken',githubToken);
-  // console.log('commentTitle',commentTitle);
+  // 이벤트가 pull_request인지 확인
+  if (eventName !== 'pull_request') {
+    core.setFailed('This action only runs on pull requests event.');
+    return;
+  }
 
-  console.log('accessToken : ', process.env.ACCESS_TOKEN);
+  try {
+    const githubToken = core.getInput('GITHUB_TOKEN');
+    const commentTitle = core.getInput('GIT_DIFF_COMMENT_TITLE');
 
-  // git diff를 가져옴
-  const gitDiffs = await getGitDiff();
-  if (!gitDiffs) return;
+    // git diff를 가져옴
+    const gitDiffs = await getGitDiff();
+    if (!gitDiffs) return;
 
-  // git diff 내용을 이메일 별로 그룹화
-  const gitDiffMap = gatherCommitsByEmail(gitDiffs);
-  const commentBody = Array.from(gitDiffMap.entries()).map(([email, commits]) => {
-    const commitStrings = commits.map(({ author_name, message, hash }) => {
-      return `- [${message}](https://github.com/${githubRepository}/pull/${prNumber}/commits/${hash})`
+    // git diff 내용을 이메일 별로 그룹화
+    const gitDiffMap = gatherCommitsByEmail(gitDiffs);
+    const commentBody = Array.from(gitDiffMap.entries()).map(([email, commits]) => {
+      const commitStrings = commits.map(({ message, hash }) => {
+        return `- [${message}](${getCommitUrl(hash)})`
+      });
+
+      const authorName = commits[0].author_name;
+      return `### ${email} <${authorName}>\n${commitStrings.join('\n')}`
+    }).join('\n\n');
+
+    // git diff를 PR 코멘트로 업데이트
+    upsertCommentInPullRequest({
+      githubToken,
+      commentBody,
+      commentTitle,
     });
-
-    const authorName = commits[0].author_name;
-    return `### ${email} <${authorName}>\n${commitStrings.join('\n')}`
-  }).join('\n\n');
-
-  // git diff를 PR 코멘트로 업데이트
-  upsertCommentInPullRequest({
-    githubToken,
-    commentBody,
-    commentTitle: '추가됨',
-  });
+  } catch (error: any) {
+    core.setFailed(`Action failed with error: ${error?.message}`);
+  }
 }
 
 run();
